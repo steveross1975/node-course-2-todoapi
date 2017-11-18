1a7ff26e0bb758f9e286812d370ae2ebf5a3c4bf
Stefano Rossetto <steveross1975@gmail.com>
2017-11-18 23:16:00 +0100

    Initial Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a10a8a798d0ed9ce59180e6')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+  //
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Stefano'
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+  //db.close();
+});
