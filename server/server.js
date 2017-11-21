6d40f04de7ae0b349697880c0abf5f1d4bbe98ef
Stefano Rossetto <steveross1975@gmail.com>
2017-11-21 21:52:43 +0100

    Added route for getting a todo by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 //First of all: start a terminal in the mongo/bin directory w/
 //./mongod --dbpath ~/Documents/Corso-NodeJS-Udemy/mongo-data/
 //command
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -32,6 +34,31 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+//GET todos/:id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    } else {
+      res.send(todo);
+    }
+  }, (e) => {
+    res.status(400).send();
+  });
+  //validate ID using isValid
+    //404 - send back empty body
+  //findById
+    //Success Case
+      // IF todo - send it back
+      //if !todo - send 404 and empty body
+    //Error Case
+      //400 - request not valid: empty body
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
