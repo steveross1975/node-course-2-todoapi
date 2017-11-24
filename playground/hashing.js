760711661b5d2393544c82cbdaf8a671a2490b7a
Stefano Rossetto <steveross1975@gmail.com>
2017-11-25 00:04:43 +0100

    Hash User pwd before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,30 @@
 const {SHA256} = require('crypto-js'); //module that gives access to password hashing functions
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-  id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
+var hashedPassword = '$2a$10$fowMvgM8Dc.38En3l1SKOelVJbMSO8c02s5W0asnRq1Ajybc5Qdl6';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded: ', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded: ', decoded);
 // var message = 'I am user number 3';
 //
 // var hash = SHA256(message).toString();
