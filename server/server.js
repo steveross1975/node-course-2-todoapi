0feb0a5f4449168eb1a05042744d58d9b5e2d45c
Stefano Rossetto <steveross1975@gmail.com>
2017-11-25 22:05:16 +0100

    Add DELETE /users/me/token route to log out users

--- server/server.js
+++ server/server.js
@@ -137,15 +137,15 @@ app.post('/users/login', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
-  // user.save().then(() => {
-  //   return user.generateAuthToken();
-  // }).then((token) => {
-  //   res.header('x-auth', token).send(user);
-  // }).catch((e) => {
-  //   res.status(400).send(e);
-  // });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
